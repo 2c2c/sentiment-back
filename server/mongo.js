c09591772d465bb0b53c0e892724e2e47744fa10
Craig Collier <tehoreoz@gmail.com>
2017-01-11 00:47:05 -0800

    production mongourl

--- server/mongo.js
+++ server/mongo.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.mongo_url)
+mongoose.connect(process.env.MONGODB_URI || process.env.mongo_url)
 
 module.exports = mongoose
