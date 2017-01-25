83db592af8e84b09fea0f4e0442186017a9daf78
Craig Collier <tehoreoz@gmail.com>
2017-01-25 01:37:49 -0800

    prettifier

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-var mongoose = require('./mongo')
-var HourlyTweets = mongoose.model('hourlytweets')
-var restify = require('restify')
-var sent = require('./sent')
+var mongoose = require("./mongo");
+var HourlyTweets = mongoose.model("hourlytweets");
+var restify = require("restify");
+var sent = require("./sent");
 
 var server = restify.createServer();
 
-server.listen(process.env.PORT || 3001, function () {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(process.env.PORT || 3001, function() {
+  console.log("%s listening at %s", server.name, server.url);
 });
 
-restify.defaultResponseHeaders = false; // disable altogether
+restify.defaultResponseHeaders = false;
 
-server.get('/', (req, res, send) => {
-  res.send('pong')
-})
+// disable altogether
+server.get("/", (req, res, send) => {
+  res.send("pong");
+});
 
-server.get('/api/hourlytweets', (req, res, send) => {
+server.get("/api/hourlytweets", (req, res, send) => {
   let query = HourlyTweets.find((err, tweet) => {
-    req.log
-    res.send(tweet)
-  })
-})
+    req.log;
+    res.send(tweet);
+  });
+});
